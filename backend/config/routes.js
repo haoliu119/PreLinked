2e1f35614f730adf237a3c83b65251d90923c0b7
Hao Liu <haoliu119@gmail.com>
2013-08-16 11:18:32 -0700

    Cleaned Install Backend using Larry's Express Boilerplate. Install Handlebar(hbs) as backend view engine

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,7 +1,12 @@
-module.exports = function(app){
+var users = require('../controllers/users.js');
+var site = require('../controllers/site.js');
 
-	//home route
-	var home = require('../controllers/home');
-	app.get('/', home.index);
+module.exports = function(app) {
+	app.get('/', site.index);
 
+	// app.post('/user', users.create);
+	// app.get('/user', users.list);
+	// app.get('/user/:id', users.read);
+	// app.put('/user/:id', users.update);
+	// app.del('/user/:id', users.delete);
 };
---
+++ backend/controllers/site.js
@@ -0,0 +1,5 @@
+var site = module.exports = {};
+
+site.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
