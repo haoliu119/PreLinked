c6d928c2b414f549009344cd38d29b06054a1b9e
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-15 17:26:41 -0700

    yo backbone --template-framework=handlebars

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global PreLinked, $*/
+
+
+window.PreLinked = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    PreLinked.init();
+});
