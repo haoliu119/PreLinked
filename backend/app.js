89afc036afae98e5024d1bd5bfdc7345adba60f6
Sportanova <sportano@gmail.com>
2013-08-16 21:26:47 -0700

    changed to nodejitsu database, removed 2 jshint occurences from Gruntfile

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ var http = require('http');
 var app = express();
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/preLinked');
+mongoose.connect('mongodb://nodejitsu:06bd14fda2df534b59f2331f41f66f26@paulo.mongohq.com:10078/nodejitsudb6063054050');
 
 require('./config/middleware.js')(app);
 require('./config/environments.js')(app);
