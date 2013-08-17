6da6ac9e899f2cf65370e07affd441cd74b62d8f
haoliu119 <haoliu119@gmail.com>
2013-08-17 12:17:55 -0700

    modified app.js, made app a global variable

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var http = require('http');
-var app = express();
 var mongoose = require('mongoose');
-
 mongoose.connect('mongodb://nodejitsu:06bd14fda2df534b59f2331f41f66f26@paulo.mongohq.com:10078/nodejitsudb6063054050');
+
+var app = express();
+global.app = app; // QUESTION-HAO: is this good practice?
 
 require('./config/middleware.js')(app);
 require('./config/environments.js')(app);
