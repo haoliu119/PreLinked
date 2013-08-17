681de5249cabd787e408eb04a954421b9a48a2f5
barry <barrymwong@yahoo.com>
2013-08-17 15:34:46 -0700

    removed conflict code. run grunt.

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -22,3 +22,21 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 
   return "<div class=\"row\">\n  <div class=\"small-10 large-centered columns\">\n    <div class=\"row collapse\">\n      <h1>Pre-Linked</h1>\n      <div class=\"small-5 columns\">\n        <label>Job</label>\n        <input type=\"text\" placeholder=\"Example: Software Engineer\">\n      </div>\n      <div class=\"small-5 columns\">\n        <label>Location</label>\n        <input type=\"text\" placeholder=\"Example: San Francisco, CA\">\n      </div>\n      <div class=\"small-2 columns\">\n        &nbsp;\n        <a href=\"#\" class=\"button prefix\">Search</a>\n      </div>\n    </div>\n  </div>\n</div>";
   });
+
+this["JST"]["app/scripts/templates/page.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "<p>Your content here.</p>\n\n";
+  });
+
+this["JST"]["app/scripts/templates/search.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "<p>Your content here.</p>\n\n";
+  });
