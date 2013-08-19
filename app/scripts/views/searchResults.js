a4e1369927185ade25f28185de460f874bd58841
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-18 22:55:40 -0700

    Add init() for collection

--- app/scripts/views/searchResults.js
+++ app/scripts/views/searchResults.js
@@ -4,6 +4,11 @@ PreLinked.Views.SearchResultsView = Backbone.View.extend({
 
   template: JST['app/scripts/templates/searchResults.hbs'],
 
+  initialize: function(){
+    this.collection.on('add', this.render, this);
+    this.collection.on('remove', this.render, this);
+  },
+
   render: function() {
     console.log('SearchResultsView render()');
     this.$el.append(this.template(this.collection));
