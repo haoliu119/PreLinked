2000416a8169c073e4dc0b69759677edc88bbeb2
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-19 01:04:54 -0700

    Now loading data from the dummy data

--- app/scripts/views/searchResults.js
+++ app/scripts/views/searchResults.js
@@ -11,7 +11,17 @@ PreLinked.Views.SearchResultsView = Backbone.View.extend({
 
   render: function() {
     console.log('SearchResultsView render()', this.collection.length);
-    this.$el.append(this.template(this.collection.length));
+    this.$el.append(this.template(
+      {jobCount: this.collection.length}
+      ));
+    this.$el.find('.jobResults').empty();
+    this.$el.find('.jobResults').append(
+      this.collection.map(function(item){
+        return new PreLinked.Views.SearchResultsItemView({
+          model: item
+        }).render().el;
+      })
+    );
     return this;
   }
 
--- app/scripts/views/searchResultsItem.js
+++ app/scripts/views/searchResultsItem.js
@@ -5,7 +5,11 @@ PreLinked.Views.SearchResultsItemView = Backbone.View.extend({
   template: JST['app/scripts/templates/searchResultsItem.hbs'],
 
   render: function() {
-    this.$el.append(this.template);
+    // this.$el.append(this.template(this.model.attributes));
+    // line above is not working with dummy data
+    this.$el.append(this.template(this.model));
+    console.log(this.model);
+    return this;
   }
 
 });
