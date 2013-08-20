d80f74b08e61aeae1fce900bfa1e2415d890db99
barry <barrymwong@yahoo.com>
2013-08-19 20:44:03 -0700

    uncomment code to get connections to work

--- backend/controllers/linkedin.js
+++ backend/controllers/linkedin.js
@@ -9,13 +9,13 @@ linkedin.search = function(req, res){
   /* dummy data for frontend work
   /*/
   // console.log("session >>>>> ", req.session);
-  // var fileContent = fs.readFileSync(path.join(__dirname, '../public/_temp_dummy_data/dummy_linkedin_connections_search_results.json'), 'utf8');
-  // res.json(fileContent);
+  var fileContent = fs.readFileSync(path.join(__dirname, '../public/_temp_dummy_data/dummy_linkedin_connections_search_results.json'), 'utf8');
+  res.json(fileContent);
 
   /* LinkedInApi endpoit
   /* TODO: check if it's html or json request
   /*/
-  LinkedInApi.search(req).then(function(json) {
-    res.json(json);
-  })
+  // LinkedInApi.search(req).then(function(json) {
+  //   res.json(json);
+  // })
 };
