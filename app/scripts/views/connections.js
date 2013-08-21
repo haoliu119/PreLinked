9c0b06465702a42eb44d1e298cbea52fb2b7ee7b
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-20 22:07:09 -0700

    LoginBox is mostly finished

--- app/scripts/views/connections.js
+++ app/scripts/views/connections.js
@@ -9,54 +9,17 @@ PreLinked.Views.ConnectionView = Backbone.View.extend({
     this.loginBox = new PreLinked.Views.LoginboxView();
   },
 
-  appendLoginModal: function(){
-    $('body').append('<div id="loginModal" class="reveal-modal">\
-<h2>Awesome. I have it.</h2>\
-<a href="http://localhost:3000/auth/linkedin">Login</a>\
-<a class="close-reveal-modal">&#215;</a>\
-</div>');
-  },
-
-  checkLogin: function(){
-    var deferred = $.Deferred();
-    $.ajax({
-      type: "GET",
-      url: "/session",
-      contentType: "application/json; charset=utf-8",
-      dataType: "json"
-    }).done(function(data){
-      deferred.resolve( JSON.parse(data) );
-    })
-    return deferred.promise();
-  },
-
   render: function(){
     console.log('connection.js -render-');
     console.log('CONNECTION RESULTS ', this.collection);
 
-    this.appendLoginModal();
-
     this.$el.html(this.template({
-      number_of_connections: this.collection.length,
-      checkLogin: false
+      number_of_connections: this.collection.length
     }));
-    //default
-    //user is NOT logged in
 
     this.$el
       .find('#login-box')
-      .empty()
-      .append(this.loginBox.render().el);
-
-    // var output = this.checkLogin();
-    // var that = this;
-    // output.done(function(data){
-    //   console.log('results from checkLogin', data);
-    //   that.$el.html(that.template({
-    //     number_of_connections: that.collection.length,
-    //     checkLogin: data
-    //   }));
-    // });
+      .html(this.loginBox.render().el);
 
     this.$el.find('#connection-results').empty();
     this.$el.find('#connection-results').append(
