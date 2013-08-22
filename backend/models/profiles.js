6f91d15ca1a1e00ca1c82e58bec151543abe33db
haoliu119 <haoliu119@gmail.com>
2013-08-21 22:09:06 -0700

    creaetd models and schemas for company, locaiton, title. modified model for jobs and profiles

--- backend/models/profiles.js
+++ backend/models/profiles.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var profileSchema = new Schema({
+  _id: { type: String, unique: true },
   searchHistory: [Schema.Types.Mixed],
   inProfile:      Schema.Types.Mixed,
   firstDegree:   [Schema.Types.ObjectId]
