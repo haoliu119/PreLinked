499bd7b0b5268d178b17ccf4b2f2b97a3f54088f
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-22 15:16:50 -0700

    Rename from profile to person

--- backend/models/persons.js
+++ backend/models/persons.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var profileSchema = new Schema({
+var personSchema = new Schema({
   _id: { type: String, unique: true }, // use LinkedIn ID
   searchHistory: [Schema.Types.Mixed],
-  inProfile:      Schema.Types.Mixed,
+  inPerson:      Schema.Types.Mixed,
   firstDegree:   [Schema.Types.ObjectId],
   secondDegree:  [Schema.Types.ObjectId],
   thirdDegree:   [Schema.Types.ObjectId]
 });
 
-module.exports = mongoose.model('Profile', profileSchema);
+module.exports = mongoose.model('Person', personSchema);
