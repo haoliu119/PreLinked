f4a092b10ca3319fa45d0556ed11b97ddb5e06a5
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-21 17:30:27 -0700

    Deal with zero job results

--- app/scripts/views/searchResults.js
+++ app/scripts/views/searchResults.js
@@ -15,13 +15,23 @@ PreLinked.Views.SearchResultsView = Backbone.View.extend({
         jobTitle: this.jobQuery.title}
     ));
 
-    this.$el.find('.jobResults').html(
-      this.collection.map(function(item){
-        return new PreLinked.Views.SearchResultsItemView({
-          model: item
-        }).render().el;
-      })
-    );
+    if(this.collection.length){
+      this.$el
+        .find('.jobResults')
+        .html(
+          this.collection.map(function(item){
+            return new PreLinked.Views.SearchResultsItemView({
+              model: item
+            }).render().el;
+          })
+        );
+    } else {
+      this.$el
+        .find('.jobResults')
+        .html("Sorry. I can't find anything for you.<br>"+
+              "Do you want a bagel instead?<br><br>");
+    }
+
     return this;
   }
 });
