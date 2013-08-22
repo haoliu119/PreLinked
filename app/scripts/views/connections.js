48407e36d483916a87c2205ab021b5f132bf0d3a
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-21 17:38:07 -0700

    Now deal with zero connection case

--- app/scripts/views/connections.js
+++ app/scripts/views/connections.js
@@ -31,14 +31,20 @@ PreLinked.Views.ConnectionView = Backbone.View.extend({
       .html(this.loginBox.render().el);
 
     if( this.collection.length ){ //if collection is NOT empty
-      this.$el.find('#connection-results').empty();
-      this.$el.find('#connection-results').append(
-        this.collection.map(function(item) {
-          return new PreLinked.Views.ConnectionsitemView({
-            model: item
-          }).render().el;
-        })
-      );
+      this.$el
+        .find('#connection-results')
+        .html(
+          this.collection.map(function(item) {
+            return new PreLinked.Views.ConnectionsitemView({
+              model: item
+            }).render().el;
+          })
+        );
+    } else {
+      this.$el
+        .find('#connection-results')
+        .html("Sorry. I can't find any connection for you.<br>"+
+              "How about adding me as your connection...<br><br>");
     }
 
     return this;
