63de35af7708f17d71cb37bb07329797bb85d0b8
haoliu119 <haoliu119@gmail.com>
2013-08-21 19:32:08 -0700

    added link button to search result item view

--- app/scripts/views/searchResultsItem.js
+++ app/scripts/views/searchResultsItem.js
@@ -2,10 +2,16 @@
 
 PreLinked.Views.SearchResultsItemView = Backbone.View.extend({
 
+  className: 'job',
+
   template: JST['app/scripts/templates/searchResultsItem.hbs'],
 
+  events: {
+    'click': ''
+  },
+
   render: function() {
-    this.$el.append(this.template(this.model.attributes));
+    this.$el.html(this.template(this.model.attributes));
     return this;
   }
 });
