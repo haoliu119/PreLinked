5fa86034bd86a2f190233f0f0ddb638328321955
haoliu119 <haoliu119@gmail.com>
2013-08-23 16:56:44 -0700

    added company filter, refactored query search string concatination, so indeed returns more accurate results

--- app/scripts/models/searchFilter.js
+++ app/scripts/models/searchFilter.js
@@ -4,6 +4,7 @@ PreLinked.Models.SearchfilterModel = Backbone.Model.extend({
 
   initialize: function() {
     this.set('jobTitle', []);
+    this.set('company', []);
     this.set('jobLocation', "");
     this.set('jobKeywords', []);
     this.on('addSearchFilter', this.addSearchFilter);
@@ -11,9 +12,11 @@ PreLinked.Models.SearchfilterModel = Backbone.Model.extend({
     this.on('removeSearchFilter', this.removeSearchFilter);
   },
 
-  addSearchFilter: function(e, title, location, keywords) {
+  addSearchFilter: function(e, title, company, location, keywords) {
     if(e.target.id === 'jobTitleSearchInput') {
       this.attributes.jobTitle.push(title);
+    } else if(e.target.id === 'companySearchInput') {
+      this.attributes.company.push(company);
     } else if(e.target.id === 'locationSearchInput') {
       this.set('jobLocation', location);
     } else if(e.target.id === 'keywordsSearchInput') {
@@ -21,9 +24,12 @@ PreLinked.Models.SearchfilterModel = Backbone.Model.extend({
     }
   },
 
-  addSearchFilterOnSubmit: function(title, location, keywords) {
+  addSearchFilterOnSubmit: function(title, company, location, keywords) {
     if(title) {
       this.attributes.jobTitle.push(title);
+    }
+    if(company) {
+      this.attributes.company.push(company);
     }
     if(location) {
       this.set('jobLocation', location);
@@ -41,13 +47,20 @@ PreLinked.Models.SearchfilterModel = Backbone.Model.extend({
       var indexToRemove = _.indexOf(jobTitleArray, elToRemove);
       jobTitleArray.splice(indexToRemove, 1);
       this.set('jobTitle', jobTitleArray);
+    } else if(filterType === 'removeCompanyFilter') {
+      var companyArray = this.get('company');
+      var indexToRemove = _.indexOf(companyArray, elToRemove);
+      companyArray.splice(indexToRemove, 1);
+      this.set('company', companyArray);
     } else if(filterType === 'removeJobLocationFilter') {
       this.set('jobLocation', '');
     } else if(filterType === 'removeJobKeywordsFilter') {
       var jobKeywordsArray = this.get('jobKeywords');
       var indexToRemove = _.indexOf(jobKeywordsArray, elToRemove);
       jobKeywordsArray.splice(indexToRemove, 1);
+      console.log('keywords after splice', jobKeywordsArray);
       this.set('jobKeywords', jobKeywordsArray);
+      console.log('keywords after set', this.get('jobKeywords'));
     }
   },
 
@@ -56,17 +69,51 @@ PreLinked.Models.SearchfilterModel = Backbone.Model.extend({
     searchQuery.location = this.get('jobLocation');
 
     var title = this.get('jobTitle');
+    var company = this.get('company');
     var keywords = this.get('jobKeywords');
 
-    searchQuery.title = title[0];
-    for(var i = 1; i < title.length; i++) {
-      searchQuery.title += ' ' + title[i];
+    if(title.length) {
+      searchQuery.title = "title:(";
+      for(var i = 0; i < title.length; i++) {
+        searchQuery.title += "'" + title[i] + "'";
+        if (i !== title.length - 1 ){
+          searchQuery.title += " or ";
+        }
+      }
+      searchQuery.title += ")";
+    } else {
+      searchQuery.title = '';
     }
+    console.log('title qued >>>>>> ', searchQuery.title);
 
-    searchQuery.keywords = keywords[0];
-    for(var i = 1; i < keywords.length; i++) {
-      searchQuery.keywords += ' ' + keywords[i];
+    if(company.length) {
+      searchQuery.company = "company:(";
+      for(var i = 0; i < company.length; i++) {
+        searchQuery.company += "'" + company[i] + "'";
+        if (i !== company.length - 1 ){
+          searchQuery.company += " or ";
+        }
+      }
+      searchQuery.company += ")";
+    } else {
+      searchQuery.company = '';
     }
+    console.log('company qued >>>>>> ', searchQuery.company);
+    console.log('keywords >>>>>> ', keywords);
+
+    if(keywords.length) {
+      searchQuery.keywords = "(";
+      for(var i = 0; i < keywords.length; i++) {
+        searchQuery.keywords += "'" + keywords[i] + "'";
+        if (i !== keywords.length - 1 ){
+          searchQuery.keywords += " or ";
+        }
+      }
+      searchQuery.keywords += ")";
+    } else {
+      searchQuery.keywords = '';
+    }
+    console.log('keywords qued >>>>>> ', searchQuery.keywords);
 
     return searchQuery;
   }
