c4459e33ca561b5c37e12dfa767b24cb6af21c9d
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-27 14:43:28 -0700

    Add restrict().

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -12,11 +12,22 @@ var persons   = require('../controllers/persons.js');
 var getdb     = require('../controllers/getDb.js');
 var users     = require('../controllers/users');
 
+//http://stackoverflow.com/questions/14218725/working-with-sessions-in-express-js
+var restrict = function(req, res, next) {
+  if (req.session && req.session.passport && req.session.passport.user) {
+    next();
+  } else {
+    console.log('Did NOT pass restrict(). Please check session.');
+    req.session.error = 'Access denied!';
+    res.redirect('/');
+  }
+};
+
 module.exports = function(app) {
   app.get('/serverindex', site.index);
 
   //getDb
-  app.get('/getdb', getdb.testKeyword);
+  app.get('/getdb', restrict, getdb.testKeyword);
 
   //test score
   app.get('/testScore', jobsSorted.testScore);
@@ -51,9 +62,9 @@ module.exports = function(app) {
   });
 
   // LinkedIn API
-  app.get('/people/search', linkedin.searchConnections);
-  app.get('/people/:id', linkedin.getProfile);
-  app.get('/people/', linkedin.searchFirstDegree);
+  app.get('/people/search', restrict, linkedin.searchConnections);
+  app.get('/people/:id', restrict, linkedin.getProfile);
+  app.get('/people/', restrict, linkedin.searchFirstDegree);
   // app.get('/people/search', linkedin.searchConnections);
   // app.get('/people/:id', persons.getById);
   // app.get('/people/', persons.get);
