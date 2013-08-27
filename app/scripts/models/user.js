20a0db444717452b182f819301276edf84e1f91a
haoliu119 <haoliu119@gmail.com>
2013-08-26 21:51:28 -0700

    created user model and view frontend

--- app/scripts/models/user.js
+++ app/scripts/models/user.js
@@ -1,29 +1,24 @@
 /*global PreLinked, Backbone*/
 
 PreLinked.Models.UserModel = Backbone.Model.extend({
-  urlRoot: '/user/searches',
+  urlRoot: '/user',
 
   defaults: {
-    jobTitle: [],
-    company: [],
-    jobLocation: '',
-    jobKeywords: [],
-    distance: 25
+    searchHistory: []
   },
 
   initialize: function() {
-    console.log('user model save');
-    // this.save({
-    //   jobTitle: ['title']
-    //   // company: ['company'],
-    //   // jobLocation: 'location',
-    //   // jobKeywords: ['keywords'],
-    //   // distance: 10
-    //   // jobTitle: this.get('title'),
-    //   // company: this.get('company'),
-    //   // jobLocation: this.get('jobLocation'),
-    //   // jobKeywords: this.get('jobKeywords'),
-    //   // distance: this.get('distance')
-    // });
+    this.fetchUser();
+  },
+
+  fetchUser: function(){
+    var that = this;
+    this.fetch()
+      .done(function(){
+        console.log('user attributes: ', that.attributes);
+      })
+      .fail(function(error){
+        console.log('user session does not exist............');
+      });
   }
 });
