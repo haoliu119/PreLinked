2239037c44f7093c1335fcdac8b94d85e4951343
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-28 21:15:00 -0700

    Add renderAttr()

--- app/scripts/views/searchRecentItem.js
+++ app/scripts/views/searchRecentItem.js
@@ -4,8 +4,12 @@ PreLinked.Views.SearchrecentitemView = Backbone.View.extend({
 
     template: JST['app/scripts/templates/searchRecentItem.hbs'],
 
+    renderAttr: function(){
+      this.$el.append(this.template(this.model.attributes));
+      return this;
+    },
+
     render: function() {
-      // this.$el.append(this.template(this.model.attributes));
       this.$el.append(this.template(this.model));
       return this;
     }
