cf24dad334afa4f0fbba5ba78ec17df34d7c0d7d
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-29 16:56:40 -0700

    Move code from session.js to util.js

--- backend/controllers/util.js
+++ backend/controllers/util.js
@@ -26,6 +26,23 @@ util.getSession = function(req, res) {
   }
 };
 
+util.setSession = function(req, res) {
+  console.log('- SESSION CONTROLLER SHOULD BE DEPRECATED >>');
+  // UserModel.findOne(
+  //   { id: req.user.id },
+  //   function(err, users){
+  //     if(err) {
+  //       console.log("error >>>>>> ", err);
+  //       res.redirect('/'); // TODO: WHERE IS REDIRECT?
+  //     } else {
+  //       // req.session.userID = req.user.id;
+  //       req.session.passport.user.accessToken = users.accessToken;
+  //       res.redirect('/#search');
+  //     }
+  //   });
+  res.redirect(404, '/#home');
+};
+
 util.logout = function(req, res) {
   req.session.destroy(function(){
     res.redirect('/');
