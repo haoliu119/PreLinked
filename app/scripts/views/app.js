66f4e2e4104cb982ecc4e2820092034ffda37411
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-28 18:14:04 -0700

    Now render based on local and remote data

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -36,7 +36,7 @@ PreLinked.Views.AppView = Backbone.View.extend({
       that.userModel.addSearchHistory();
       //
       that.searchView.renderSearchRecentBasedOnFrontendData(that.userModel.get('searchHistory'));
-      // that.searchView.renderSearchRecent();
+      that.searchView.renderSearchRecent();
 
     }, this);
     this.homeView = new PreLinked.Views.HomeView({
