d845ed1223ef0a2dd214eb675e9b4290d4b771fd
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-29 15:09:35 -0700

    Delete persons._put, shao

--- backend/controllers/persons.js
+++ backend/controllers/persons.js
@@ -111,49 +111,6 @@ persons._post = function(data, myId){
   return deferred.promise;
 };
 
-persons._put = function(data, myId){
-  var deferred = Q.defer();
-  // console.log('_put data: ', data);
-
-  var query = Person.findOne({_id: data.id});
-  // query.exec(function(error, oldPerson){
-  //   if(error){
-  //     console.log('Unable to find person?\n', error);
-  //     deferred.reject(error);
-  //     return deferred.promise;
-  //   }
-
-  //   if(oldPerson){
-  //     //update the person
-  //     console.log('Find the oldPerson, start, \n', data);
-  //     console.log('Find the oldPerson, end. \n');
-  //     oldPerson.update({
-  //       $set: {inPerson: data}
-  //       // $addToSet: {firstDegree: myId}
-  //       //todo
-  //       //check for whether it is a first degree or not
-  //     },function(err){
-  //       if(err){
-  //         console.log(err);
-  //         deferred.reject(err);
-  //       }else{
-  //         console.log("Successfully updated\n");
-  //         deferred.resolve(oldPerson);
-  //       }
-  //     });
-
-  //   } else {
-  //     //save as a new person
-  //     console.log('Save a new Person\n');
-  //     persons._post(data, myId)
-  //       .then(function(data){
-  //         deferred.resolve(data);
-  //       });
-  //   }
-  // });
-
-  return deferred.promise;
-};
 
 persons.put = function(req, res){
   var deferred = Q.defer();
