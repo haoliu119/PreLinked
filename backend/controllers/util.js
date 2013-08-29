6af332fec464a6b6e9f31b5c86cfa12359b563bc
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-29 15:05:33 -0700

    Refactor code out of routes.js

--- backend/controllers/util.js
+++ backend/controllers/util.js
@@ -1,4 +1,16 @@
 var util = module.exports = {};
+var passport = require('passport');
+
+//http://stackoverflow.com/questions/14218725/working-with-sessions-in-express-js
+util.restrict = function(req, res, next) {
+  if (req.session && req.session.passport && req.session.passport.user) {
+    next();
+  } else {
+    console.log('Did NOT pass restrict(). Please check session.');
+    req.session.error = 'Access denied!';
+    res.redirect('/');
+  }
+};
 
 util.getSession = function(req, res) {
   if(req.session && req.session.passport && req.session.passport.user){
@@ -16,3 +28,23 @@ util.logout = function(req, res) {
     // res.send(200, 'You are logged out!');
   });
 };
+
+util.authLinkedinPassport = passport.authenticate(
+                              'linkedin',
+                              {
+                                scope: ['r_fullprofile', 'r_network'],
+                                state: '12345'
+                              }
+                            );
+
+util.authLinkedinFunc = function(req, res) {
+  //this empty funciton is required by passport
+};
+
+util.authLinkedinCallback = passport.authenticate(
+                              'linkedin',
+                              {
+                                successRedirect: '/#search',
+                                failureRedirect: '/#home'
+                              }
+                            );
