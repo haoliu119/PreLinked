9580b497b421512806e27bc9f270ff1f2e571d39
Sportanova <sportano@gmail.com>
2013-08-29 20:57:46 -0700

    removed x (click to exit) from top right of modal

--- app/scripts/views/modalConnectionDetails.js
+++ app/scripts/views/modalConnectionDetails.js
@@ -4,12 +4,6 @@ PreLinked.Views.ModalconnectiondetailsView = Backbone.View.extend({
 
   template: JST['app/scripts/templates/modalConnectionDetails.hbs'],
 
-  events: {
-    'click .exitModal': function() {
-      $('.exitModal').foundation('reveal', 'close');
-    }
-  },
-
   render: function(){
     this.$el.append( this.template(this.model));
     return this;
